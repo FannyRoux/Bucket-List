ac44ec8c842d55cae0f388a1bae109cd34598692
FannyRoux <fannyrouxfr@gmail.com>
2024-02-17 18:06:59 +0100

    First page

--- bucket-list/src/Controller/MainController.php
+++ bucket-list/src/Controller/MainController.php
@@ -3,8 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-
+    #[Route('/main', name: 'app_main')]
+    public function home():Response
+    {
+        return $this->render('main/home.html.twig');
+    }
 }
