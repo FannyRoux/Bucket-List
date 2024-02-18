1dad9f920d50de66c3abcce21726777b319297eb
FannyRoux <fannyrouxfr@gmail.com>
2024-02-18 13:56:55 +0100

    add a wish (form, template, entity)

---
+++ bucket-list/src/Controller/WishController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Wish;
+use App\Form\WishType;
+use App\Repository\WishRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+#[Route ('/wish', name:'wish_')]
+class WishController extends AbstractController
+{
+    #[Route('/wishList', name: 'list')]
+    public function wishlist(WishRepository $wishRepository): Response
+    {
+        $wishes = $wishRepository->findBy(['isPublished'=>true],['dateCreated'=>'DESC']);
+        return $this->render('wish/wishList.html.twig',[
+            'wishes'=>$wishes
+        ]);
+    }
+
+    #[Route('/wishDetails/{id}', name:'details')]
+    public function whishDetails(int $id, WishRepository $wishRepository):Response
+    {
+        $wish= $wishRepository->find($id);
+        return $this->render('wish/wishDetails.html.twig',[
+            'wish'=>$wish
+        ]);
+    }
+
+    #[Route('/newWish', name:'new')]
+    public function addWish(EntityManagerInterface $entityManager, Request $request):Response
+    {
+        $newWish = new Wish();
+        $newWish->setDateCreated(new DateTime());
+        $newWish->setIsPublished(true);
+        $newWishForm = $this->createForm(WishType::class,$newWish);
+
+        $newWishForm->handleRequest($request);
+        if($newWishForm->isSubmitted() && $newWishForm->isValid()){
+            $entityManager->persist($newWish);
+            $entityManager->flush();
+
+            $this->addFlash('success','New wish created !');
+            return $this->redirectToRoute('wish_details', ['id'=> $newWish->getId()]);
+        }
+        return $this->render('wish/newWish.html.twig', [
+            'newWishForm'=>$newWishForm->createView()
+        ]);
+    }
+}
---
+++ bucket-list/src/Entity/Wish.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WishRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Length;
+
+#[ORM\Entity(repositoryClass: WishRepository::class)]
+class Wish
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 250)]
+    #[Assert\Length(min: 5, minMessage: "Are you kindding ? That's way far to be a good title ! Try again")]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 500)]
+    #[Assert\Length(min: 10, minMessage: "Make a little effort, be more specific... Let us dream !")]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\Length(min: 2, minMessage: "Is that really your name ??? Don't think so, try again !")]
+    private ?string $author = null;
+
+    #[ORM\Column]
+    private ?bool $isPublished = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateCreated = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function isIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): static
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated(\DateTimeInterface $dateCreated): static
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+}
